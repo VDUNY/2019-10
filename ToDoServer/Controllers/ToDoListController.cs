9f642e90e066a8ebb426055e8f33648c5906aa6d
Joel Bennett <Jaykul@HuddledMasses.org>
2019-10-13 00:04:22 -0400

    OpenAPI ToDoServer

---
+++ ToDoServer/Controllers/ToDoListController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ToDoServer.Models;
+
+namespace ToDoServer.Controllers
+{
+    // [ApiVersion("1.0")]
+    [Route("[controller]")]
+    public class ToDoListController : ControllerBase
+    {
+        private static Dictionary<int, ToDoTask> mockData = new Dictionary<int, ToDoTask>();
+
+        static ToDoListController()
+        {
+            mockData.Add(0, new ToDoTask { Id = 0, Owner = "*", Description = "feed the dog" });
+            mockData.Add(1, new ToDoTask { Id = 1, Owner = "*", Description = "take the dog on a walk" });
+        }
+
+        /// <summary>
+        /// Get a (list of) <see cref="ToDoTask"/> for the specified user
+        /// </summary>
+        /// <param name="owner">The user to filter by</param>
+        /// <param name="id">An id to retrieve only one <see cref="ToDoTask"/></param>
+        /// <returns>A (potentially empty) collection of <see cref="ToDoTask"/></returns>
+        [HttpGet("{owner}", Name = "GetList")]
+        public IEnumerable<ToDoTask> Get([FromRoute]string owner)
+        {
+            return mockData.Values.Where(m => m.Owner == owner || owner == "*");
+        }
+
+        /// <summary>
+        /// Get a (list of) <see cref="ToDoTask"/> for the specified user
+        /// </summary>
+        /// <param name="owner">The user to filter by</param>
+        /// <param name="id">An id to retrieve only one <see cref="ToDoTask"/></param>
+        /// <returns>A (potentially empty) collection of <see cref="ToDoTask"/></returns>
+        [HttpGet("{owner}/{id}", Name = "GetTask")]
+        public ToDoTask GetTask([FromRoute]string owner, [FromRoute]int id)
+        {
+            return mockData.Values.Where(m => (m.Owner == owner || owner == "*" ) && m.Id == id).First();
+        }
+
+        /// <summary>
+        /// Add a <see cref="ToDoTask"/>
+        /// </summary>
+        /// <param name="todo">The <see cref="ToDoTask"/> to add</param>
+        [HttpPost]
+        public ActionResult<ToDoTask> Post(ToDoTask todo)
+        {
+            todo.Id = mockData.Count > 0 ? mockData.Keys.Max() + 1 : 1;
+            mockData.Add(todo.Id, todo);
+
+            return CreatedAtRoute("GetTask", new { owner = todo.Owner, id = todo.Id }, todo);
+        }
+
+        /// <summary>
+        /// Modify a <see cref="ToDoTask"/>
+        /// </summary>
+        /// <param name="todo">The <see cref="ToDoTask"/> with a modified description</param>
+        [HttpPut]
+        public void Put(ToDoTask todo)
+        {
+            ToDoTask xtodo = mockData.Values.First(a => (a.Owner == todo.Owner || todo.Owner == "*") && a.Id == todo.Id);
+            if (todo != null && xtodo != null)
+            {
+                xtodo.Description = todo.Description;
+            }
+        }
+
+        /// <summary>
+        /// Delete a <see cref="ToDoTask"/>
+        /// </summary>
+        /// <param name="owner">The user that owns the <see cref="ToDoTask"/> to delete</param>
+        /// <param name="id">The id of the <see cref="ToDoTask"/> to delete</param>
+        [HttpDelete]
+        public void Delete([FromRoute]string owner, [FromRoute]int id)
+        {
+            ToDoTask todo = mockData.Values.First(a => (a.Owner == owner || owner == "*") && a.Id == id);
+            if (todo != null)
+            {
+                mockData.Remove(todo.Id);
+            }
+        }
+    }
+}
+
---
+++ ToDoServer/Startup.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+[assembly: ApiController]
+namespace ToDoServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            // Register the Swagger services
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "ToDo API";
+                    document.Info.Description = "A simple ASP.NET Core web API";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "Joel Bennett",
+                        Email = "Jaykul@HuddledMasses.org",
+                        Url = "https://twitter.com/jaykul"
+                    };
+                    document.Info.License = new NSwag.OpenApiLicense
+                    {
+                        Name = "Use under the MIT License",
+                        Url = "https://opensource.org/licenses/mit-license.html"
+                    };
+                };
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
